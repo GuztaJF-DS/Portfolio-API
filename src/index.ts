d7af1d745073192eb4edbb97c90cb569d829e650
GuztaJF-DS <noobshippuden13@gmail.com>
2023-09-04 12:15:22 -0300

    feat: added eb config

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/portfolio');
+    const conn = await mongoose.connect(process.env.MONGO_URL as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
