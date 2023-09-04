c20d8122196a7b476a4ff57abc2542bdfb2335ab
GuztaJF-DS <noobshippuden13@gmail.com>
2023-09-04 11:01:32 -0300

    feat: added config for elasticbeanstalk

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL as string);
+    const conn = await mongoose.connect('mongodb://localhost:27017/portfolio');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
@@ -17,6 +17,8 @@ const connectDB = async () => {
 
 connectDB().then(() => {
   server.listen((process.env.PORT as string) || 8080, async () => {
-    console.info(('server running on port ' + process.env.PORT) as string);
+    console.info(
+      ('server running on port ' + (process.env.PORT || 8080)) as string
+    );
   });
 });
